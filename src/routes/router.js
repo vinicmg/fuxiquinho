5158e6b26730c8daa157836b37d8e29546683577
vinicmg <viniciuscamargo61@gmail.com>
2021-06-15 23:04:03 -0300

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,21 +6,27 @@ import NewGossip from '../pages/NewGossip/NewGossip.vue'
 
 Vue.use(VueRouter)
 
+const ItWorks = {
+    template: '<div>It works!</div>'
+  },
+
 const routes = [
+    
+    { path: '/fuxiquinho', name: 'root', component: ItWorks },
+    // {
+    //     path: '/',
+    //     name: 'home',
+    //     component: Home
+    // },
     {
-        path: '/',
-        name: 'home',
-        component: Home
-    },
-    {
-        path: '/nova',
+        path: '/fuxiquinho/nova',
         name: 'nova',
         component: NewGossip
     }
 ];
 
 const router = new VueRouter({
-    mode: "history",
+    mode: "hash",
     routes    
   });
 
