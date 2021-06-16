c7ac6bab19a549f5d9bd07e79df4d292f1fd4de8
vinicmg <viniciuscamargo61@gmail.com>
2021-06-15 23:52:29 -0300

    MediaQuery

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,148 +1,153 @@
 <template>
-    <header>
-        <nav class="container">
-            <a href="/">
-                <img alt="logo" id="logo" src="../../assets/logo.png" />
-            </a>
+  <header>
+    <nav class="container">
+      <a href="/">
+        <img alt="logo" id="logo" src="../../assets/logo.png" />
+      </a>
 
-            <h2>Fuxiquinho</h2>
+      <h2>Fuxiquinho</h2>
 
-            <img v-on:click="openMenu" alt="menu" id="menu-button" src="../../assets/menu.png" />
+      <img
+        v-on:click="openMenu"
+        alt="menu"
+        id="menu-button"
+        src="../../assets/menu.png"
+      />
 
-            <div v-on:click="closeMenu" id="menu-overlay" v-if="menuActive"></div>
-            <div id="menu-items" :class="{active:menuActive}">
-
-                <img alt="menu-logo" id="menu-logo" src="../../assets/logo.png" />
-                <ul>    
-                    <router-link to="/">Inicio</router-link>        
-                    <router-link to="/nova">Nova Fofoquinha</router-link>        
-                    <router-link to="/sobre">Sobre</router-link>        
-                    <router-link to="/contato">Contato</router-link>        
-                </ul>
-            </div>
-        </nav>
-    </header>
+      <div v-on:click="closeMenu" id="menu-overlay" v-if="menuActive"></div>
+      <div id="menu-items" :class="{ active: menuActive }">
+        <img alt="menu-logo" id="menu-logo" src="../../assets/logo.png" />
+        <ul>
+          <router-link to="/">Inicio</router-link>
+          <router-link to="/nova">Nova Fofoquinha</router-link>
+          <router-link to="/sobre">Sobre</router-link>
+          <router-link to="/contato">Contato</router-link>
+        </ul>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
-    name: 'Header',
-    data() {
-        return {
-            menuActive: false
-        }
+  name: "Header",
+  data() {
+    return {
+      menuActive: false,
+    };
+  },
+  methods: {
+    openMenu: function () {
+      this.menuActive = true;
     },
-    methods: {
-        openMenu: function() {
-            this.menuActive = true
-        },
-        closeMenu: function() {
-            this.menuActive = false
-        }
-    }
-}
+    closeMenu: function () {
+      this.menuActive = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
-    header {
-        background-color: orange;
-        height: 60px;
-        width: 100%;
-        display: flex;
-        justify-content: center;
-    }
+header {
+  background-color: orange;
+  height: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
 
-    nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        height: 60px;
-    }
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+}
 
-    #logo {
-        width: 50px;
-    }
+#logo {
+  width: 50px;
+}
 
-    #menu-logo {
-        width: 100px;
-        margin-top: 30px;
-        margin-bottom: 10px;
-    }
+#menu-logo {
+  width: 100px;
+  margin-top: 30px;
+  margin-bottom: 10px;
+}
 
-    #menu-button {
-        width: 30px;
-        height: 30px;
-    }
+#menu-button {
+  width: 30px;
+  height: 30px;
+}
 
-    #menu-overlay {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 40%;
-        height: 100vh;
-        background-color: #000000;
-        opacity: 0.8;
-    }
+#menu-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 40%;
+  height: 100vh;
+  background-color: #000000;
+  opacity: 0.8;
+}
 
-    #menu-items {
-        position: fixed;
-        top: 0;
-        right: 0;
-        background-color: orange;
-        width: 60%;
-        height: 100vh;
-        display: none;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: center;
-    }
+#menu-items {
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: orange;
+  width: 60%;
+  height: 100vh;
+  display: none;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
 
-    #menu-items.active {
-        display: flex;
-    }
+#menu-items.active {
+  display: flex;
+}
 
-    ul {
-        list-style: none;    
-        text-align: center;
-    }
+ ul {
+  margin: 20px 0px;
+}
 
-    ul li {
-        margin: 20px 0px;
-    }
+ ul a {
+  color: white;
+  margin-left: 10px;
+  text-decoration: none;
+}
 
-    ul li a {
-        color: white;
-    }
+h2 {
+  color: white;
+  font-family: Verdana;
+}
 
-    h2 {
-        color: white;
-        font-family: Verdana;
-    }
+.router-link-active {
+    color: white;
+}
 
-    @media (min-width: 700px) {
-        #menu-button,
-        #menu-logo,
-        #menu-overlay {
-            display: none;
-        }
+@media screen and (min-width: 700px) {
+  #menu-button,
+  #menu-logo,
+  #menu-overlay {
+    display: none;
+  }
 
-        #menu-items {
-            display: flex;
-            position: static;
-            height: 60px;
-            width: auto;
-        }
+  #menu-items {
+    display: flex;
+    position: static;
+    height: 60px;
+    width: auto;
+  }
 
-        ul {
-            display: flex;
-            flex-direction: row;
-            height: 60px;
-            align-items: center;
-        }
+  ul {
+    display: flex;
+    flex-direction: row;
+    height: 60px;
+    align-items: center;
+  }
 
-        ul  li {
-            margin: 0;
-            margin-left: 20px;            
-        }
-    }
+  ul li {
+    margin: 0;
+    margin-left: 20px;
+  }
+}
 </style>
